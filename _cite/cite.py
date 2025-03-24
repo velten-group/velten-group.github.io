003554d30d8d95e74d191fc341cf1bc30b3a3190
Thomas Greulich <thomas.greulich@cos.uni-heidelberg.de>
2025-03-24 11:14:14 +0100

    correct copy _cite/cite.py

--- _cite/cite.py
+++ _cite/cite.py
@@ -13,9 +13,6 @@ from util import *
 load_dotenv()
 
 
-# error flag
-error = False
-=======
 # save errors/warnings for reporting at end
 errors = []
 warnings = []
@@ -45,12 +42,6 @@ for plugin in plugins:
     files = Path.cwd().glob(f"_data/{plugin.stem}*.*")
     files = list(filter(lambda p: p.suffix in [".yaml", ".yml", ".json"], files))
 
-    log(f"Found {len(files)} {plugin.stem}* data file(s)", 1)
-
-    # loop through data files
-    for file in files:
-        log(f"Processing data file {file.name}", 1)
-=======
     log(f"Found {len(files)} {plugin.stem}* data file(s)", indent=1)
 
     # loop through data files
@@ -62,59 +53,35 @@ for plugin in plugins:
             data = load_data(file)
             # check if file in correct format
             if not list_of_dicts(data):
-
-                raise Exception("File not a list of dicts")
-        except Exception as e:
-            log(e, 2, "ERROR")
-            error = True
-=======
                 raise Exception(f"{file.name} data file not a list of dicts")
         except Exception as e:
             log(e, indent=2, level="ERROR")
             errors.append(e)
-
             continue
 
         # loop through data entries
         for index, entry in enumerate(data):
-
-            log(f"Processing entry {index + 1} of {len(data)}, {label(entry)}", 2)
-=======
             log(f"Processing entry {index + 1} of {len(data)}, {label(entry)}", level=2)
-
 
             # run plugin on data entry to expand into multiple sources
             try:
                 expanded = import_module(f"plugins.{plugin.stem}").main(entry)
                 # check that plugin returned correct format
                 if not list_of_dicts(expanded):
-
-                    raise Exception("Plugin didn't return list of dicts")
-=======
                     raise Exception(f"{plugin.stem} plugin didn't return list of dicts")
-
             # catch any plugin error
             except Exception as e:
                 # log detailed pre-formatted/colored trace
                 print(traceback.format_exc())
                 # log high-level error
-
-                log(e, 3, "ERROR")
-                error = True
-=======
                 log(e, indent=3, level="ERROR")
                 errors.append(e)
-
                 continue
 
             # loop through sources
             for source in expanded:
                 if plugin.stem != "sources":
-
-                    log(label(source), 3)
-=======
                     log(label(source), level=3)
-
 
                 # include meta info about source
                 source["plugin"] = plugin.name
@@ -124,11 +91,7 @@ for plugin in plugins:
                 sources.append(source)
 
             if plugin.stem != "sources":
-
-                log(f"{len(expanded)} source(s)", 3)
-=======
                 log(f"{len(expanded)} source(s)", indent=3)
-
 
 
 log("Merging sources by id")
@@ -141,11 +104,7 @@ for a in range(0, len(sources)):
     for b in range(a + 1, len(sources)):
         b_id = get_safe(sources, f"{b}.id", "")
         if b_id == a_id:
-
-            log(f"Found duplicate {b_id}", 2)
-=======
             log(f"Found duplicate {b_id}", indent=2)
-
             sources[a].update(sources[b])
             sources[b] = {}
 sources = [entry for entry in sources if entry]
@@ -166,8 +125,6 @@ citations = []
 for index, source in enumerate(sources):
     log(f"Processing source {index + 1} of {len(sources)}, {label(source)}")
 
-
-=======
     # if explicitly flagged, remove/ignore entry
     if get_safe(source, "remove", False) == True:
         continue
@@ -180,8 +137,6 @@ for index, source in enumerate(sources):
 
     # Manubot doesn't work without an id
     if _id:
-        log("Using Manubot to generate citation", 1)
-=======
         log("Using Manubot to generate citation", indent=1)
 
         try:
@@ -190,16 +145,6 @@ for index, source in enumerate(sources):
 
         # if Manubot cannot cite source
         except Exception as e:
-            # if regular source (id entered by user), throw error
-            if get_safe(source, "plugin", "") == "sources.py":
-                log(e, 3, "ERROR")
-                error = True
-            # otherwise, if from metasource (id retrieved from some third-party API), just warn
-            else:
-                log(e, 3, "WARNING")
-                # discard source from citations
-                # continue
-=======
             plugin = get_safe(source, "plugin", "")
             file = get_safe(source, "file", "")
             # if regular source (id entered by user), throw error
@@ -236,18 +181,6 @@ try:
     save_data(output_file, citations)
 except Exception as e:
     log(e, level="ERROR")
-    error = True
-
-
-# exit at end, so user can see all errors in one run
-if error:
-    log("Error(s) occurred above", level="ERROR")
-    exit(1)
-else:
-    log("All done!", level="SUCCESS")
-
-log("\n")
-=======
     errors.append(e)
 
 
